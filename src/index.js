84f51c7a7432358c668007c0cfc3875d417af80e
pchynoweth <24738364+pchynoweth@users.noreply.github.com>
2020-05-23 15:14:53 +0100

    Fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export { default as default } from './ShareButton.svelte';
 export { default as Email} from './Email.svelte';
 export { default as Facebook } from './Facebook.svelte';
-export { default as LinkedIn } './LinkedIn.svelte';
-export { default as Reddit } './Reddit.svelte';
-export { default as Twitter } './Twitter.svelte';
+export { default as LinkedIn } from './LinkedIn.svelte';
+export { default as Reddit } from './Reddit.svelte';
+export { default as Twitter } from './Twitter.svelte';
