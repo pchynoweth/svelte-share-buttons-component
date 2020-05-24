cf67ca30f48c32ca3916b98807c168a4eac71c64
pchynoweth <24738364+pchynoweth@users.noreply.github.com>
2020-05-24 19:22:06 +0100

    Removed default export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-export { default as default } from './ShareButton.svelte';
 export { default as Email} from './Email.svelte';
 export { default as Facebook } from './Facebook.svelte';
 export { default as LinkedIn } from './LinkedIn.svelte';
