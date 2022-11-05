c41ac892b78c1f70d215c32001021ed034054c2c
pchynoweth <24738364+pchynoweth@users.noreply.github.com>
2022-11-05 14:07:34 +0000

    Fixed upgrade issue

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
-import pkg from './package.json';
+import pkg from './package.json' assert{ type: 'json' };
 
 const name = pkg.name
 	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
